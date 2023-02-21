cd81b4188ec85cdcd49d2a4a8ff114af1001b15c
Kuyabongeka22 <maezar28@outlook.com>
2023-02-20 17:08:42 -0800

    puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - start execution
+ * return: 0 for success
+ */
+int main(void)
+{
+        puts("\"Programming is like building a multilingual puzzle\n");
+        return (0);
+}
