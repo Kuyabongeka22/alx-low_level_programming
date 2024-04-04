4cca0f8e7ae3d537b390c720cbd7807510a521b1
Kuyabongeka <mayezakuyabongeka@outlook.com>
2024-04-04 13:22:55 -0700

    4.

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,21 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-/**
- * main - This is the entry point of the code
- * @argc: stores the number of command-line arguments passed
- * @argv: an array of character pointers listing all the arguments.
- * Return: 0 Successful
- */
-int main(int argc, char **argv)
-{
-	int i;
-	int sum = 0;
+#include <string.h>
+#include <ctype.h>
+#include "main.h"
 
-	for (i = 1; i < argc; ++i)
+/**
+ * main - Program that takes in all integer arguments and returns the sum
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 1 if a non-integer is among the passed in arguments, 0 otherwise
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j, length, sum;
+	char *ptr;
+
+	if (argc < 2)
+		printf("0\n");
+	else
 	{
-		sum = sum + atoi(argv[i]);
-		printf("%d\n", sum);
+		sum = 0;
+		for (i = 1; i < argc; i++)
+		{
+			ptr = argv[i];
+			length = strlen(ptr);
+
+			for (j = 0; j < length; j++)
+			{
+				if (isdigit(*(ptr + j)) == 0)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+
+			sum += atoi(argv[i]);
+		}
+
+	printf("%d\n", sum);
 	}
-return (0);
+	return (0);
 }
