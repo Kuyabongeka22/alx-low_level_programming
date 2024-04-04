dfb565459ae935f970d2a2a0a10e683fd177349a
Kuyabongeka <mayezakuyabongeka@outlook.com>
2024-04-04 07:36:13 -0700

    2.

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - This is the entry point of the code
+ * @argc: stores the number of command-line arguments passed
+ * @argv: an array of character pointers listing all the arguments.
+ * Return: 0 Successful
+ */
+int main(int argc, char **argv)
+{
+	int i = 0;
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+return (0);
+}
