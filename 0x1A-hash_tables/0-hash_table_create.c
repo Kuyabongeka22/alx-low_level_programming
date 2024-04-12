b2dd7cd3eae130a0da59c55887d13d7537a54b59
Kuyabongeka <mayezakuyabongeka@outlook.com>
2024-04-12 13:12:15 -0700

    2.

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -20,10 +20,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	newTable->size = size;
 
-	newTable->array = malloc(sizeof(hash_node_t *) * size);
+	newTable->array = malloc(sizeof(hash_node_t*) * size);
 	if (newTable->array == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
 		newTable->array[i] = NULL;
-return (newTable);
+
+	return (newTable);
 }
